f350b5329da348ce50e461c919c5e055d6cf1872
Jorge Marques <jorge-marques@jorge.marques>
2021-05-13 00:12:34 -0300

    commit inicial

---
+++ src/main/java/com/jorgemarques/mentoriaapi/MentoriaApiApplication.java
@@ -0,0 +1,13 @@
+package com.jorgemarques.mentoriaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MentoriaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MentoriaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jorgemarques/mentoriaapi/MentoriaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jorgemarques.mentoriaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MentoriaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
